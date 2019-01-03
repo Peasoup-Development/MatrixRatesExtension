f27bc7dc15d461c9ba4c2ed7206a5f504edb6d1d
Peasoup-Development <robert.williamson1986@googlemail.com>
2019-01-03 21:51:24 +0000

    Update UninstallSchema.php

--- Setup/UninstallSchema.php
+++ Setup/UninstallSchema.php
@@ -14,19 +14,17 @@ class UninstallSchema implements UninstallInterface
     )
     {
         $setup->startSetup();
-            // Version of module in setup table is less then the give value.
 
 
-            // get table customer_entity
+            $installer = $setup;
+
             $eavTable = $setup->getTable('webshopapps_matrixrate');
 
-            // Check if the table already exists
             if ($setup->getConnection()->isTableExists($eavTable) == true) {
-                $connection = $setup->getConnection();
 
-                $connection->dropColumn($eavTable, 'condition_name2');
-                $connection->dropColumn($eavTable, 'condition2_from_value');
-                $connection->dropColumn($eavTable, 'condition2_to_value');
+
+                $installer->getConnection()->dropTable($installer->getConnection()->getTableName('webshopapps_matrixrate'));
+
             }
 
             $setup->endSetup();
